efae9257e9567fafa35ddeb8276c9e28287540ff
Michael Stucki <michael.stucki@cs2.ch>
2013-10-24 23:54:06 +0200

    Compatibility fix for TYPO3 6.x

--- cli/class.tx_cleartypo3cache_cli_cli.php
+++ cli/class.tx_cleartypo3cache_cli_cli.php
@@ -10,7 +10,9 @@
 
 if (!defined ('TYPO3_cliMode')) die ('Access denied: CLI only.');
 
-require_once(PATH_t3lib.'class.t3lib_cli.php');
+if (version_compare(TYPO3_branch, '6.0', '<')) {
+	require_once(PATH_t3lib.'class.t3lib_cli.php');
+}
 
 /**
  * class to process actions via CLI
